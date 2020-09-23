73336118c5b97e5baf96431f5b43d2f2387efdea
Patricio Soto Kort <patosoto@gmail.com>
2020-09-23 19:20:33 +0800

    Adds suburbs list

--- fuelwatch.py
+++ fuelwatch.py
@@ -1,13 +1,21 @@
 from flask import Flask, escape, request, render_template
 from services.stations import getStations
-#from modules.views import createHeader, createBody, createFooter, stationsTable
+import json
+import random
 
 app = Flask(__name__)
+with open('server_assets/australian_postcodes.json') as f:
+    suburbs = json.load(f)
+
+# Filter to leave only WA suburbs
+suburbs = [s for s in suburbs if s['state'] == 'WA']
 
 @app.route('/')
 def main():
-    stations = getStations('como')
-    return render_template('index.html', stations=stations, suburb='Como')
+    suburb = random.choice(suburbs)
+    subname = escape(suburb['locality']).capitalize()
+    stations = getStations(subname)
+    return render_template('index.html', stations=stations, suburb=subname)
 
 @app.route('/<suburb>')
 def per_suburb(suburb):
