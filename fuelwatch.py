53da97805f1ad4e5efeebe058866c99d2ed95080
Patricio Soto Kort <psotokort@gmail.com>
2020-09-23 21:12:58 +0800

    Changes suburb list and add selection in template

--- fuelwatch.py
+++ fuelwatch.py
@@ -1,27 +1,26 @@
 from flask import Flask, escape, request, render_template
 from services.stations import getStations
-import json
 import random
 
 app = Flask(__name__)
-with open('server_assets/australian_postcodes.json') as f:
-    suburbs = json.load(f)
+# Read suburbs from file
+with open('server_assets/suburbs.txt') as f:
+    suburbs = f.readlines()
 
-# Filter to leave only WA suburbs
-suburbs = [s for s in suburbs if s['state'] == 'WA']
+# Trim, Capitalize and Escape each suburb
+suburbs = [escape(s.rstrip().capitalize()) for s in suburbs]
 
 @app.route('/')
 def main():
     suburb = random.choice(suburbs)
-    subname = escape(suburb['locality']).capitalize()
-    stations = getStations(subname)
-    return render_template('index.html', stations=stations, suburb=subname)
+    stations = getStations(suburb)
+    return render_template('index.html', stations=stations, suburb=suburb, suburbs=suburbs)
 
 @app.route('/<suburb>')
 def per_suburb(suburb):
     sub = escape(suburb).capitalize()
     stations = getStations(sub)
-    return render_template('index.html', stations=stations, suburb=sub)
+    return render_template('index.html', stations=stations, suburb=sub, suburbs=suburbs)
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
